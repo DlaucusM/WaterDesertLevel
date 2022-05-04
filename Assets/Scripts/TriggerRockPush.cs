711ed746ed3ef92f8c717f964295b6f1b25760d9
DlaucusM <tylermoss478@gmail.com>
2022-05-04 17:01:41 +1000

    end of day working on section 2

---
+++ Assets/Scripts/TriggerRockPush.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerRockPush : MonoBehaviour
+{
+    public GameObject objectToMove;
+    public GameObject particalEffect;
+    public GameObject particalEffect2;
+    private List<int> combo = new List<int> { 5, 3, 2, 4, 1 };
+    private List<int> currentCombo = new List<int> {};
+    private bool pass;
+
+    public void ComboMetre(int importValue)
+    {
+        currentCombo.Add(importValue);
+        Debug.Log(currentCombo.Count);
+        if (currentCombo.Count > 4)
+        {
+
+            pass = true;
+            for (int i = 0; i < 5; i++)
+            {
+                if (currentCombo[currentCombo.Count-i-1] != combo[i])
+                {
+                   pass = false;
+                    Debug.Log(currentCombo[currentCombo.Count-i-1]);
+
+                }
+               /* Debug.Log(currentCombo[0]);
+                Debug.Log(currentCombo[1]);
+                Debug.Log(currentCombo[2]);
+                Debug.Log(currentCombo[3]);
+                Debug.Log(currentCombo[4]);
+               */
+            }
+            if (pass == true)
+            {
+                objectToMove.GetComponent<Animator>().SetTrigger("RockPush");
+                particalEffect.SetActive(true);
+                particalEffect2.SetActive(true);
+            }
+        }
+    }
+}
